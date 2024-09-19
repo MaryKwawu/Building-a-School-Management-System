e585b1d59ceacb036dc7cf18d77cac98abf2fc1a
MaryKwawu <may419316@gmail.com>
2024-09-19 12:58:57 +0000

    feat/studentObject : created a student object

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,11 @@ import java.util.List;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
         /**
          * creating Teacher Object
          */
+
         int teacherId1 = 1;
         String teacherName1= "Marie";
         int teacherSalary1 = 20000;
@@ -31,6 +33,30 @@ public class Main {
         teacherList.add(marie);
         teacherList.add(wiseborn);
         teacherList.add(joseph);
+
+/**
+ * created student object
+ */
+        int studentId1 = 2;
+        String studentName1 = "Melisa";
+        int studentGrade1 = 6;
+        Student Melisa = new Student(studentId1, studentName1 , studentGrade1);
+
+        int studentId2 = 2;
+        String studentName2 = "Lauren";
+        int studentGrade2 = 7;
+        Student Lauren = new Student(studentId2, studentName2, studentGrade2);
+
+        int studentId3 = 2;
+        String studentName3 = "Loren";
+        int studentGrade3 = 8;
+        Student Loren = new Student(studentId3, studentName3, studentGrade3);
+
+        List <Student> studentList = new ArrayList<>();
+        studentList.add(Melisa);
+        studentList.add(Lauren);
+        studentList.add(Loren);
+
 
 
 
